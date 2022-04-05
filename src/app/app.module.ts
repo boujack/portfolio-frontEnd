60794a910d5262dcaf02db9d5b2b3e56f4c82392
boujack <valdes.bou@gmail.com>
2022-04-05 12:27:40 -0300

    añadidos botones de login y menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginButtonComponent } from './components/login-button/login-button.component';
+import { MenuButtonComponent } from './components/menu-button/menu-button.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavbarComponent
+    NavbarComponent,
+    LoginButtonComponent,
+    MenuButtonComponent
   ],
   imports: [
     BrowserModule
