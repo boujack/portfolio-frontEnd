8e48761c33944e0aab9e09c7937cf096b309085a
boujack <valdes.bou@gmail.com>
2022-04-05 12:41:25 -0300

    añadido el banner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { HeaderComponent } from './components/header/header.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { LoginButtonComponent } from './components/login-button/login-button.component';
 import { MenuButtonComponent } from './components/menu-button/menu-button.component';
+import { BannerComponent } from './components/banner/banner.component';
+import { PlacaComponent } from './components/placa/placa.component';
+import { PerfilComponent } from './components/perfil/perfil.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +16,10 @@ import { MenuButtonComponent } from './components/menu-button/menu-button.compon
     HeaderComponent,
     NavbarComponent,
     LoginButtonComponent,
-    MenuButtonComponent
+    MenuButtonComponent,
+    BannerComponent,
+    PlacaComponent,
+    PerfilComponent
   ],
   imports: [
     BrowserModule
