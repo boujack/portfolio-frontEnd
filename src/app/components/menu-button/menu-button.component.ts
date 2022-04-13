5e846684ef10380820f39d2b94ac0543f190223b
boujack <valdes.bou@gmail.com>
2022-04-13 14:05:18 -0300

    add menu func

--- src/app/components/menu-button/menu-button.component.ts
+++ src/app/components/menu-button/menu-button.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit } from '@angular/core';
-import { faBars } from '@fortawesome/free-solid-svg-icons';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+import { faBars} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-menu-button',
@@ -8,9 +8,15 @@ import { faBars } from '@fortawesome/free-solid-svg-icons';
 })
 export class MenuButtonComponent implements OnInit {
   faBars=faBars;
+  @Output() menu:EventEmitter<boolean> = new EventEmitter;
+  private status:boolean=false;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  menuToggle(){
+    this.status=!this.status
+    this.menu.emit(this.status);
+  }
 }
